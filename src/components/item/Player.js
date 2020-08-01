e72083718a2550c05458eb869b22cde54f98bdf6
Afzaal B <afzaalopera@gmail.com>
2020-08-02 00:56:00 +0500

    trying

--- src/components/item/Player.js
+++ src/components/item/Player.js
@@ -1,107 +1,127 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Measure from "react-measure";
 import YTPlayer from "react-youtube";
 import ReactImageFallback from "react-image-fallback";
-import Play from "react-bytesize-icons/Play";
 import { FALLBACK_IMAGE, IMAGE_URL, youTubeConfig } from "../../constants";
-import { Spring } from "react-spring/renderprops";
+import { Spring, animated } from "react-spring/renderprops";
 
-const Player = ({ videoId, poster, title, playing, handlePlayerState }) => {
-  const [dimensions, setDimensions] = useState({});
-
-  let playerWidth = Math.round((window.innerWidth * 80) / 100);
-  let playerHeight = Math.round((playerWidth / 16) * 9);
-
-  if (window.innerHeight < window.innerWidth) {
-    playerHeight = Math.round((window.innerHeight * 80) / 100);
-    playerWidth = Math.round((playerHeight / 9) * 16);
-  }
-
-  let trailerSpring = {
-    from: {
-      transform: "translate3d(0px, 0px, 0)",
-    },
-    to: {
-      height: "auto",
-      width: "auto",
-    },
+class Player extends React.Component {
+  state = {
+    dimensions: {},
   };
-  document.body.classList.remove("modal-open");
 
-  if (playing) {
-    const horzOrigin = Math.round(
-      (window.innerWidth - playerWidth) / 2 - dimensions.left
-    );
-    const vertOrigin = Math.round(
-      (window.innerHeight - playerHeight) / 2 - dimensions.top
-    );
-    trailerSpring = {
-      ...trailerSpring,
-      to: {
-        height: playerHeight,
-        width: playerWidth,
-        transform: `translate3d(${horzOrigin}px, ${vertOrigin}px, 0)`,
-      },
-    };
+  setDimensions = (dimensions) => {
+    this.setState({ dimensions });
+  };
 
-    // Prevent body scroll
-    document.body.classList.add("modal-open");
+  componentDidUpdate(prevProps) {
+    const { playing } = this.props;
   }
 
-  return (
-    <Spring {...trailerSpring} reset>
-      {(animatedSpring) => (
-        <Measure
-          bounds
-          onResize={(contentRect) => {
-            setDimensions(contentRect.bounds);
-          }}
-        >
-          {({ measureRef }) => (
-            <section
-              className={`player-section c-hand position-relative ${
-                playing && "is-playing"
-              }`}
+  render() {
+    const { videoId, poster, title, playing, handlePlayerState } = this.props;
+    const { dimensions } = this.state;
+
+    let playerWidth = Math.round((window.innerWidth * 80) / 100);
+    let playerHeight = Math.round((playerWidth / 16) * 9);
+
+    if (window.innerHeight < window.innerWidth) {
+      playerHeight = Math.round((window.innerHeight * 80) / 100);
+      playerWidth = Math.round((playerHeight / 9) * 16);
+    }
+
+    const horzOrigin =
+      ((window.innerWidth - playerWidth) / 2).toFixed(2) - dimensions.left;
+    const vertOrigin =
+      ((window.innerHeight - playerHeight) / 2).toFixed(2) - dimensions.top;
+
+    if (playing) {
+      document.body.classList.add("modal-open");
+    } else {
+      document.body.classList.remove("modal-open");
+    }
+
+    return (
+      <Measure
+        bounds
+        onResize={(contentRect) => {
+          this.setDimensions(contentRect.bounds);
+        }}
+      >
+        {({ measureRef }) => (
+          <section
+            className={`player-section c-hand position-relative ${
+              playing && "is-playing"
+            }`}
+          >
+            <Spring
+              from={{
+                transform: `translate(0px, 0px)`,
+              }}
+              to={{
+                transform: playing
+                  ? `translate(${horzOrigin}px, ${vertOrigin}px)`
+                  : `translate(0px, 0px)`,
+              }}
             >
-              <div
-                ref={measureRef}
-                onClick={handlePlayerState}
-                style={animatedSpring}
-                className="over-shadow yt-player embed-responsive embed-responsive-16by9 overflow-hidden"
-              >
-                <YTPlayer
-                  videoId={videoId}
-                  opts={youTubeConfig}
-                  containerClassName="yt-container"
-                  className="embed-responsive-item"
-                />
-                {!playing && (
-                  <ReactImageFallback
-                    src={`${IMAGE_URL + "/w780" + poster}`}
-                    fallbackImage={FALLBACK_IMAGE}
-                    alt={title}
-                    className="cover-fit w-100 h-100 position-absolute"
-                  />
-                )}
-              </div>
-              {!playing && (
+              {(props) => (
                 <div
+                  style={props}
+                  ref={measureRef}
                   onClick={handlePlayerState}
-                  className="pl-icon position-absolute p-4 m-4"
+                  className="over-shadow yt-player embed-responsive embed-responsive-16by9 overflow-hidden"
                 >
-                  <Play width="36" height="36" />
+                  <div ref={measureRef}>
+                    <YTPlayer
+                      videoId={videoId}
+                      opts={youTubeConfig}
+                      containerClassName="yt-container"
+                      className="embed-responsive-item"
+                    />
+                    {!playing && (
+                      <ReactImageFallback
+                        src={`${IMAGE_URL + "/w780" + poster}`}
+                        fallbackImage={FALLBACK_IMAGE}
+                        alt={title}
+                        className="cover-fit w-100 h-100 position-absolute"
+                      />
+                    )}
+                  </div>
                 </div>
               )}
-              <div
-                onClick={handlePlayerState}
-                className="position-fixed w-100 h-100 player-bd"
-              />
-            </section>
-          )}
-        </Measure>
-      )}
-    </Spring>
-  );
-};
+            </Spring>
+            <div
+              onClick={handlePlayerState}
+              className="pl-icon position-absolute p-4 m-4"
+            >
+              <svg
+                id={playing ? "i-pause" : "i-play"}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 36 36"
+                width="36"
+                height="36"
+                fill="none"
+                stroke="currentcolor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+              >
+                <path
+                  d={
+                    playing ? "M2 30 L30 2 M30 30 L2 2" : "M10 2 L10 30 24 16 Z"
+                  }
+                />
+              </svg>
+            </div>
+            <div
+              onClick={handlePlayerState}
+              className="position-fixed w-100 h-100 player-bd"
+            />
+          </section>
+        )}
+      </Measure>
+    );
+  }
+}
 
 export default Player;
